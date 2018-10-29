120a430218abf3493d7c8cb091c792af12e884a1
amirnakic <anakic1@etf.unsa.ba>
2018-10-30 00:33:44 +0100

    Dodavanje getera klasi FiksniBroj.

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -1,6 +1,10 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 public class FiksniBroj extends TelefonskiBroj {
+
+    private String broj;
+    private Grad grad;
+
     public enum Grad {
         TRAVNIK(30), ORASJE(31), ZENICA(32), SARAJEVO(33), LIVNO(34), TUZLA(35),
         MOSTAR(36), BIHAC(37), GORAZDE(38), POSUSJE(39), BRCKO(49);
@@ -15,6 +19,11 @@ public class FiksniBroj extends TelefonskiBroj {
         }
     }
 
-    private String broj;
-    private Grad grad;
+    public String getBroj() {
+        return broj;
+    }
+
+    public Grad getGrad() {
+        return grad;
+    }
 }
