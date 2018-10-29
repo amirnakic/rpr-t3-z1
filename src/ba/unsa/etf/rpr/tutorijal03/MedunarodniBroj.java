a0b891f150e32bbc88d0002ec6eb330fde4048f3
amirnakic <anakic1@etf.unsa.ba>
2018-10-30 00:52:33 +0100

    Dodavanje getera izvedene klase MedunarodniBroj.

--- src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -3,4 +3,11 @@ package ba.unsa.etf.rpr.tutorijal03;
 public class MedunarodniBroj extends TelefonskiBroj {
     private String drzava, broj;
 
+    public String getDrzava() {
+        return drzava;
+    }
+
+    public String getBroj() {
+        return broj;
+    }
 }
