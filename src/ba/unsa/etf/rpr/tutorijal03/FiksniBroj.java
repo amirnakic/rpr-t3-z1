4b42304a911ec9fd684e51c3c85e7f068121d85b
amirnakic <anakic1@etf.unsa.ba>
2018-10-30 00:30:52 +0100

    Dodavanje pobrojanog tipa enum.

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -1,7 +1,13 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 public class FiksniBroj extends TelefonskiBroj {
-    
+    public enum Grad {
+        TRAVNIK(30), ORASJE(31), ZENICA(32), SARAJEVO(33), LIVNO(34), TUZLA(35),
+        MOSTAR(36), BIHAC(37), GORAZDE(38), POSUSJE(39), BRCKO(49);
+        private final int pozivniBroj;
+
+    }
+
     private String broj;
     private Grad grad;
 }
