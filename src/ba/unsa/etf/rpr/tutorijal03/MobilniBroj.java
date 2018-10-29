d8f1068cfb0135267ca4d78f3c7fe9cd3ac9c8e9
amirnakic <anakic1@etf.unsa.ba>
2018-10-30 00:46:06 +0100

    Dodavanje getera klase MobilniBroj.

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -1,7 +1,15 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
 public class MobilniBroj extends TelefonskiBroj {
-    
+
     private int mobilnaMreza;
     private String broj;
+
+    public int getMobilnaMreza() {
+        return mobilnaMreza;
+    }
+
+    public String getBroj() {
+        return broj;
+    }
 }
