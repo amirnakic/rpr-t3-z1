9ff69ada36936b6c99bc2dce02570a5e0a9a5cb7
amirnakic <anakic1@etf.unsa.ba>
2018-10-30 00:13:32 +0100

    Korekcije nad apstraktnom klasom TelefonskiBroj.

--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -2,13 +2,7 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 public abstract class TelefonskiBroj {
 
-    protected String broj;
-
-    TelefonskiBroj() {
-        broj = "Nepoznat broj";
-    }
-
     public abstract String ispisi();
-    
+
     public abstract int hashCode();
 }
