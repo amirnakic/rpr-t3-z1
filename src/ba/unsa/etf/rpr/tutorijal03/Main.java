1b5fe4b7c341faf756961816da88a41132010a68
amirnakic <anakic1@etf.unsa.ba>
2018-10-30 18:20:49 +0100

    Dodavanje menija unutar klase Main.

--- src/ba/unsa/etf/rpr/tutorijal03/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Main.java
@@ -1,29 +1,90 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Cilj vježbe je napraviti aplikaciju \"ETF Telefonski imenik\". Aplikacija se treba sastojati od sljedećih klasa:\n" +
-				"Apstraktna klasa TelefonskiBroj sadrži javnu apstraktnu metodu String ispisi() koja vraća broj u tekstualnoj formi pogodnoj za ispis, te apstraktnu metodu int hashCode() objašnjenu na predavanjima. Iz ove klase izvedene su sljedeće klase:\n" +
-				"FiksniBroj sadrži konstruktor FiksniBroj(Grad grad, String broj). Parametar broj je dio telefonskog broja bez pozivnog broja npr. \"123-456\", a grad je promjenljiva pobrojanog tipa koji označava pozivni broj koji treba koristiti npr.:\n" +
-				"\tenum Grad { SARAJEVO, TUZLA, ZENICA…}\n" +
-				"Spisak pozivnih brojeva se može naći ovdje. Umjesto imena kantona koristite ime glavnog grada kantona (kao u primjeru iznad), a umjesto Brčko distrikta stavite BRCKO. Metoda ispisi() treba vratiti broj oblika \"033/123-456\".\n" +
-				"MobilniBroj ima konstruktor MobilniBroj(int mobilnaMreza, String broj) pri čemu oznaka mobilne mreže predstavlja drugu i treću cifru pozivnog broja tj. 60-67 (detaljnije informacije možete naći na istoj stranici). Metoda ispisi vraća broj istog oblika \"061/987-654\".\n" +
-				"MedunarodniBroj ima konstruktor MedunarodniBroj(String drzava, String broj) pri čemu string drzava sadrži kompletan pozivni broj za državu npr. \"+387\".\n" +
-				"Klasa Imenik omogućuje da se pretražuju brojevi koristeći klasu HashMap. Ova klasa treba sadržavati sljedeće javne metode:\n" +
-				"void dodaj(String ime, TelefonskiBroj broj)\n" +
-				"String dajBroj(String ime) - vraća telefonski broj osobe pod imenom ime u formi stringa pozivajući metodu ispisi() klase TelefonskiBroj,\n" +
-				"String naSlovo(char s) - vraća sve brojeve u telefonskom imeniku za osobe čije ime počinje na slovo s u formatu:\n" +
-				"1. Ime Prezime - broj\n" +
-				"2. Ime Prezime - broj\n" +
-				"…\n" +
-				"Set<String> izGrada(Grad g) - vraća skup Stringova koji sadrži imena i prezimena svih osoba koje žive u gradu g (istog pobrojanog tipa koji je dat u klasi FiksniBroj), što se može saznati ako osoba ima fiksni broj telefona u tom gradu. Pri tome skup treba biti sortiran abecedno.\n" +
-				"Set<TelefonskiBroj> izGradaBrojevi(Grad g) - vraća brojeve za osobe iz grada g. Ovaj skup treba biti sortiran po stringu koji se dobije metodom ispisi().\n" +
-				"Klasa Program treba sadržavati main metodu koja korisniku omogućuje da unosom naredbi pomoću tastature pozove sve metode klasa navedenih iznad.\n" +
-				"\n" +
-				"Dodajte nedostajuće testove po potrebi. Prilikom rješavanja zadatka obavezno se pridržavati pravila rada koja su navedena na predavanjima, između ostalog:\n" +
-				"ispravno imenovanje paketa, klasa, metoda i promjenljivih,\n" +
-				"primjenu komentara,\n" +
-				"atomic commits.\n");
+        boolean istina = true;
+        Imenik imenik = new Imenik();
+        String imeIPrezime = new String();
+        String grad = new String();
+        String broj = new String();
+        String drzava = new String();
+        int mobilnaMreza;
+        while (istina) {
+            System.out.println("-----ETF - Telefonski imenik-----");
+            System.out.println("Izaberite koju opciju želite:");
+            System.out.println(" 0 - Prekini program");
+            System.out.println(" 1 - Dodaj broj");
+            System.out.println(" 2 - Pronađi broj na osnovu imena korisnika");
+            System.out.println(" 3 - Pronađi korisnika na osnovu broja");
+            System.out.println(" 4 - Ispiši korisnike sa unesenim početnim slovom");
+            System.out.println(" 5 - Ispiši korisnike iz unesenog grada");
+            System.out.println(" 6 - Ispiši brojeve iz unesenog grada");
+            Scanner ulaz = new Scanner(System.in);
+            int izbor = ulaz.nextInt();
+            ulaz.nextLine();
+            switch (izbor) {
+                case 0:
+                    System.out.println("Kraj programa.");
+                    istina = false;
+                    break;
+                case 1:
+                    System.out.println("-----Izabrali ste opciju dodavanja broja u imenik-----");
+                    boolean novaIstina = true;
+                    while (novaIstina) {
+                        System.out.println("Izaberite koji broj želite dodati: ");
+                        System.out.println(" 0 - Nazad");
+                        System.out.println(" 1 - Dodaj fiksni broj");
+                        System.out.println(" 2 - Dodaj mobilni broj");
+                        System.out.println(" 3 - Dodaj međunarodni broj");
+                        int noviIzbor = ulaz.nextInt();
+                        ulaz.nextLine();
+                        switch (noviIzbor) {
+                            case 0:
+                                novaIstina = false;
+                                break;
+                            case 1:
+                                System.out.println("-----Izabrali ste opciju dodavanja fiksnog broja u imenik-----");
+                                System.out.println("Unesite ime i prezime korisnika: ");
+                                imeIPrezime = ulaz.nextLine();
+                                System.out.println("Unesite grad: ");
+                                grad = ulaz.nextLine();
+                                System.out.println("Unesite telefonski broj: ");
+                                broj = ulaz.nextLine();
+                                TelefonskiBroj tb = new FiksniBroj(FiksniBroj.Grad.valueOf(grad.toUpperCase()), broj);
+                                imenik.dodaj(imeIPrezime, tb);
+                                System.out.println("Korisnik " + imeIPrezime + " sa brojem telefona " + tb.ispisi() + " je uspješno dodan.");
+                                break;
+                            case 2:
+                                System.out.println("-----Izabrali ste opciju dodavanja mobilnog broja u imenik-----");
+                                System.out.println("Unesite ime i prezime korisnika: ");
+                                imeIPrezime = ulaz.nextLine();
+                                System.out.println("Unesite mobilnu mrežu: ");
+                                mobilnaMreza = ulaz.nextInt();
+                                System.out.println("Unesite telefonski broj: ");
+                                broj = ulaz.nextLine();
+                                TelefonskiBroj mb = new MobilniBroj(mobilnaMreza, broj);
+                                imenik.dodaj(imeIPrezime, mb);
+                                System.out.println("Korisnik " + imeIPrezime + " sa brojem telefona " + mb.ispisi() + " je uspješno dodan.");
+                                break;
+                            case 3:
+                                System.out.println("-----Izabrali ste opciju dodavanja međunarodnog broja u imenik-----");
+                                System.out.println("Unesite ime i prezime korisnika: ");
+                                imeIPrezime = ulaz.nextLine();
+                                System.out.println("Unesite državu: ");
+                                drzava = ulaz.nextLine();
+                                System.out.println("Unesite telefonski broj: ");
+                                broj = ulaz.nextLine();
+                                TelefonskiBroj mb = new MedunarodniBroj(drzava, broj);
+                                imenik.dodaj(imeIPrezime, mb);
+                                System.out.println("Korisnik " + imeIPrezime + " sa brojem telefona " + mb.ispisi() + " je uspješno dodan.");
+                                break;
+                        }
+                    }
+                break;
+            }
+        }
     }
 }
