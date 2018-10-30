674289645a44c787089bce5358142a7926368904
amirnakic <anakic1@etf.unsa.ba>
2018-10-30 11:03:49 +0100

    Implementiranje generičkog interfejsa Comparable i preklapanje metode compareTo.

--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -1,8 +1,13 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public abstract class TelefonskiBroj {
+public abstract class TelefonskiBroj implements Comparable<TelefonskiBroj> {
 
     public abstract String ispisi();
 
     public abstract int hashCode();
+
+    @Override
+    public int compareTo(TelefonskiBroj tb) {
+        return this.ispisi().compareTo(tb.ispisi());
+    }
 }
